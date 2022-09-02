4b502de85ee9212bd3e763807d527d4488fc19d4
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-02 15:32:57 +0200

    created methods to call instance variables

--- class_php/movies.php
+++ class_php/movies.php
@@ -10,9 +10,28 @@ class Movie
 
     function __construct($_title, $_description, $_date, $_film_company)
     {
-        $this->title = $_title;
-        $this->description = $_description;
-        $this->date = $_date;
-        $this->film_company = $_film_company;
+        $this->getTitle($_title);
+        $this->getDescription($_description);
+        $this->getDate($_date);
+        $this->getFilmCompany($_film_company);
+    }
+
+    // Creo i method per le assegnazioni delle variabili d'istanza
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    public function getDate()
+    {
+        return $this->date;
+    }
+    public function getFilmCompany()
+    {
+        return $this->film_company;
     }
 }
