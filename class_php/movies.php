808d1c3f8dcbeb8febed7f600433e7dc11544638
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-02 17:05:50 +0200

    created setter method

--- class_php/movies.php
+++ class_php/movies.php
@@ -9,18 +9,42 @@ class Movie
 
     // Creo un costrutto per identificare con sicurezza le variabili d'istanza create
 
+
     function __construct($_title, $_description, $_date, $_film_company, $_film_poster)
     {
         // Creo le assegnazioni da fare
-        $this->title = $_title;
-        $this->description = $_description;
-        $this->date = $_date;
-        $this->film_company = $_film_company;
-        $this->film_poster = $_film_poster;
+        $this->setTitle($_title);
+        $this->setDescription($_description);
+        $this->setDate($_date);
+        $this->setFilmCompany($_film_company);
+        $this->setFilmPoster($_film_poster);
     }
 
-    // Creo i method per le assegnazioni delle variabili d'istanza
+    // Creo i method per le assegnazioni delle variabili d'istanza le creo con il setter in modo da impostarle 
 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+    public function setFilmCompany($film_company)
+    {
+        $this->film_company = $film_company;
+    }
+    public function setFilmPoster($film_poster)
+    {
+        $this->film_poster = $film_poster;
+    }
+
+
+    // Creo i method per ottenere le variabili d'istanza con il metodo getter
     public function getTitle()
     {
         return $this->title;
